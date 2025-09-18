9fbe91bde6972176b88d2784c9363f1dad50729a
Ashish Kumar Singh <69068290+Ashish-kumar2020@users.noreply.github.com>
2025-09-18 06:59:45 +0530

    design user schema

---
+++ backend/models/user.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+  name: {
+    type: String,
+    required: true,
+    trim: false,
+  },
+  profilePicture: {
+    type: String,
+  },
+  isEmailVerified: {
+    type: Boolean,
+    default: false,
+  },
+  lastLogin: {
+    type: Date,
+  },
+  is2FAEnabled: {
+    type: Boolean,
+    default: false
+  },
+  twoFAOtp: {
+    type: String,
+
+    select: false
+  },
+  twoFAOtpExpire: {
+    type: Date,
+    select: false
+  }
+},{timestamps: true});
+
+
+const User = mongoose.model("User",userSchema);
+export default User;
