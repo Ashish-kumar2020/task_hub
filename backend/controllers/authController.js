a50d345d507452496e7cdcbc781648a8261e236a
Ashish Kumar Singh <69068290+Ashish-kumar2020@users.noreply.github.com>
2025-09-24 07:26:28 +0530

    Added email verification

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,38 +1,77 @@
 import User from "../models/user.js";
 import bcrypt, { genSalt } from "bcrypt"
+import jwt from "jsonwebtoken";
+import Verification from "../models/verification.js";
+import { sendEmail } from "../libs/send-email.js";
+import aj from "../libs/arcjet.js";
 
 
-
-const registerUser = async (req,res) => {
+const registerUser = async (req, res) => {
     try {
-        const {email,name,password} = req.body;
-
-        const existingUser = await User.findOne({email});
-        if(existingUser){
-            res.status(400).json({
-                message: "Email address already in use"
-            })
-        }
-        
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(password,salt);
-
-        const newUser = await User.create({
-            email,
-            password: hashedPassword,
-            name
+      const { email, name, password } = req.body;
+  
+      const decision = await aj.protect(req, { email });
+      console.log("Arcjet decision", decision.isDenied());
+  
+      if (decision.isDenied()) {
+        res.writeHead(403, { "Content-Type": "application/json" });
+        res.end(JSON.stringify({ message: "Invalid email address" }));
+      }
+  
+      const existingUser = await User.findOne({ email });
+  
+      if (existingUser) {
+        return res.status(400).json({
+          message: "Email address already in use",
         });
-
-        // TODO : send email
-        res.status(201).json({
-            message: "Verification Email Sent to your email. Please check and verify your account.",
-        })
+      }
+  
+      const salt = await bcrypt.genSalt(10);
+  
+      const hashPassword = await bcrypt.hash(password, salt);
+  
+      const newUser = await User.create({
+        email,
+        password: hashPassword,
+        name,
+      });
+  
+      const verificationToken = jwt.sign(
+        { userId: newUser._id, purpose: "email-verification" },
+        process.env.JWT_SECRET,
+        { expiresIn: "1h" }
+      );
+  
+      await Verification.create({
+        userId: newUser._id,
+        token: verificationToken,
+        expiresAt: new Date(Date.now() + 1 * 60 * 60 * 1000),
+      });
+  
+      // send email
+      const verificationLink = `${process.env.FRONTEND_URL}/verify-email?token=${verificationToken}`;
+      const emailBody = `<p>Click <a href="${verificationLink}">here</a> to verify your email</p>`;
+      const emailSubject = "Verify your email";
+  
+      console.log("EMail",email)
+      const isEmailSent = await sendEmail(email, emailSubject, emailBody);
+  
+      if (!isEmailSent) {
+        return res.status(500).json({
+          message: "Failed to send verification email",
+        });
+      }
+  
+      res.status(201).json({
+        message:
+          "Verification email sent to your email. Please check and verify your account.",
+      });
     } catch (error) {
-        console.log(error);
-        res.status(500).json({message: "Internal Server Error"});
+      console.log(error);
+  
+      res.status(500).json({ message: "Internal server error" });
     }
-    
-}
+  };
 const loginUser = async (req,res) => {
 
     try {
